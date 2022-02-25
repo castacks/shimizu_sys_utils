d8cd72d40a650c93da5e08bbd47f030768bb7b21
yaoyuh <yaoyuh@andrew.cmu.edu>
2022-02-24 21:27:43 -0500

    Add memory monitor.

---
+++ include/shimizu_sys_utils/memory.hpp
@@ -0,0 +1,59 @@
+
+#ifndef __SHIMIZU_SYS_UTILS_MEMORY_HPP___
+#define __SHIMIZU_SYS_UTILS_MEMORY_HPP___
+
+#include <iostream>
+#include <string>
+
+namespace shimizu
+{
+
+/**
+ * @brief MemoryMonitor class.
+ * 
+ * See https://man7.org/linux/man-pages/man5/proc.5.html for more information.
+ * 
+ */
+class MemoryMonitor {
+
+public:
+    MemoryMonitor();
+    MemoryMonitor(bool human_readable);
+    ~MemoryMonitor();
+
+    void set_human_readable(bool b) { human_readable = b; }
+    MemoryMonitor& update();
+
+    /**
+     * @brief Return the peak RSS in KB.
+     * 
+     * @return long the peak RSS (VmHWM)
+     */
+    long peak_rss() const { return _peak_rss; }
+
+    /**
+     * @brief Return the RSS is KB.
+     * 
+     * @return long the RSS (VmRSS)
+     */
+    long rss() const { return _rss; }
+
+    std::string string() const;
+
+    friend std::ostream& operator << (std::ostream& os, const MemoryMonitor& mm);
+
+private:
+    std::pair<double, std::string> convert_2_human_readable( long original ) const;
+
+private:
+    const int MEM_BASE = 1024;
+
+    bool human_readable;
+
+    long _peak_rss; // KB, VmHWM.
+    long _rss; // KB, VmRSS.
+};
+
+} // namespace shimizu
+
+#endif // __SHIMIZU_SYS_UTILS_MEMORY_HPP___
---
+++ src/memory.cpp
@@ -0,0 +1,95 @@
+
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+
+#include "shimizu_sys_utils/memory.hpp"
+
+using namespace shimizu;
+
+MemoryMonitor::MemoryMonitor()
+: human_readable(false), _peak_rss(0)
+{}
+
+MemoryMonitor::MemoryMonitor(bool human_readable)
+: human_readable(human_readable), _peak_rss(0)
+{}
+
+MemoryMonitor::~MemoryMonitor()
+{}
+
+static
+long parse_line(const std::string& line) {
+    std::string name;
+    long value = 0;
+    std::stringstream ss(line);
+    ss >> name >> value;
+    return value;
+}
+
+MemoryMonitor& MemoryMonitor::update() {
+    _peak_rss = 0;
+    _rss = 0;
+    
+    std::ifstream ifs("/proc/self/status");
+
+    if (ifs) {
+        std::string line;
+        bool have_VmHWM = false;
+        bool have_VmRSS = false;
+        while ( std::getline(ifs, line) ) {
+            if ( !have_VmHWM && line.find("VmHWM") != std::string::npos ) {
+                _peak_rss = parse_line(line);
+                have_VmHWM = true;
+            }
+
+            if ( !have_VmRSS && line.find("VmRSS") != std::string::npos ) {
+                _rss = parse_line(line);
+                have_VmRSS = true;
+            }
+
+            if ( have_VmHWM && have_VmRSS ) break;
+        }
+    }
+
+    return *this;
+}
+
+std::pair<double, std::string> 
+MemoryMonitor::convert_2_human_readable( long original ) const {
+    auto value = static_cast<double>( original ); // KB;
+
+    if ( value < MEM_BASE ) {
+        return { value, "KB" };
+    }
+
+    value /= MEM_BASE;
+    if ( value < MEM_BASE ) {
+        return { value, "MB" };
+    }
+
+    value /= MEM_BASE;
+    // Only as large as GB.
+    return { value, "GB" };
+}
+
+std::string MemoryMonitor::string() const {
+    std::stringstream ss;
+    ss << std::fixed;
+
+    if ( human_readable ) {
+        const auto peak_rss = convert_2_human_readable( _peak_rss );
+        const auto rss = convert_2_human_readable( _rss );
+
+        ss << std::setprecision(2) << rss.first << "/" << peak_rss.first << " " << peak_rss.second;
+    } else {
+        ss << std::setprecision(2) << _rss << "/" << _peak_rss << " KB";
+    }
+
+    return ss.str();
+}
+
+std::ostream& shimizu::operator << (std::ostream& os, const MemoryMonitor& mm) {
+    os << mm.string();
+    return os;
+}
